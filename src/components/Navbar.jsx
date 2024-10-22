6c3d8abe4dc8f084729fbf978652730062369242
saikamal@45 <saikamal2727@gmail.com>
2024-10-22 18:29:15 +0530

    Added media Queries

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 export const Navbar = () => {
+
+  const [isOpen,setIsOpen]=useState(false);
+
+  const toggleMenu=()=>{
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu=()=>{
+  setIsOpen(false);
+  };
+
   return (
-    <nav>
+    <nav className={styles.navbar}>
       <h2>Xyz Institute</h2>
-      <ul className={styles.navbg}>
+      <button className={styles.menuButton} onClick={toggleMenu}>
+      <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+      </button>
+      <ul className={`${styles.navbg} ${isOpen ? styles.showMenu : ''}`}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={closeMenu}>Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about" onClick={closeMenu}>About</Link>
         </li>
         <li>
-          <Link to="/courses">Courses</Link>
+          <Link to="/courses" onClick={closeMenu}>Courses</Link>
         </li>
         <li>
-          <Link to="/contact">ContactUs</Link>
+          <Link to="/contact" onClick={closeMenu}>ContactUs</Link>
         </li>
       </ul>
     </nav>
